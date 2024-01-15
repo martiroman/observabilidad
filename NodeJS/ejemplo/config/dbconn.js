aa8a5eaf1b12efecba8c1a7faac12f7f503cf792
martiroman <martinnahuel.roman@protonmail.com>
2024-01-15 12:35:02 -0300

    configuration

--- NodeJS/ejemplo/config/dbconn.js
+++ NodeJS/ejemplo/config/dbconn.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(`mongodb://10.40.0.23:27017/mitienda`)
+  .connect(`mongodb://tiendapc-svc.test-nodejs-app:27017/mitienda`)
   .then(() => {
     console.log("Conectado a la DB mitienda");
   })
