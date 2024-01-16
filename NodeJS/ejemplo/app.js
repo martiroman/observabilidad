b143def3e2cf7de8e74c6e4932cd2c0aa90e5872
martiroman <martinnahuel.roman@protonmail.com>
2024-01-16 12:44:21 -0300

    change test

--- NodeJS/ejemplo/app.js
+++ NodeJS/ejemplo/app.js
@@ -4,7 +4,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-
 const indexRouter = require("./routes/index");
 const productoRouter = require("./routes/producto");
 const usuarioRouter = require("./routes/usuario");
